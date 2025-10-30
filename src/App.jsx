945f641424d8d92e98a2398d0844eff4ad1b0bbc
sercansimsek <sercansimsek44@gmail.com>
2025-10-30 14:30:39 +0100

    data fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import reactionImg from "./assets/images/icon-reaction.svg";
-import memoryImg from "./assets/images/icon-memory.svg";
-import verbalImg from "./assets/images/icon-verbal.svg";
-import visualImg from "./assets/images/icon-visual.svg";
-
-import dataFromServer from "./data/data.json";
+import cn from "classnames";
+import dataFromServer from "../data.json";
 
 export const App = () => {
-  console.log(dataFromServer);
-
   return (
     <div className="flexCenter bg-main min-h-screen font-hanken">
       <div className="bg-white w-[327px]">
@@ -33,50 +27,39 @@ export const App = () => {
           <span className="font-semibold text-lg text-darkGrayBlue mb-5">
             Summary
           </span>
-          <div className="flex flex-col gap-3 mb-5">
-            <div className="resultCard bg-lightRedBcg ">
-              <div className="flex gap-3 text-lightRed ">
-                <img src={reactionImg} alt="reaction icon" />
-                <span>Reaction</span>
-              </div>
-              <div>
-                <span className="text-darkGrayBlue">80</span>{" "}
-                <span className="text-darkGrayBlueFaded"> / 100</span>
-              </div>
-            </div>
-
-            <div className="resultCard bg-orangeYellowBcg">
-              <div className="flex gap-3 text-orangeYellow">
-                <img src={memoryImg} alt="memory icon" />
-                <span>Memory</span>
-              </div>
-              <div>
-                <span className="text-darkGrayBlue">80</span>{" "}
-                <span className="text-darkGrayBlueFaded"> / 100</span>
-              </div>
-            </div>
-
-            <div className="resultCard bg-greenTealBcg">
-              <div className="flex gap-3 text-greenTeal">
-                <img src={verbalImg} alt="verbal icon" />
-                <span>Reaction</span>
-              </div>
-              <div>
-                <span className="text-darkGrayBlue">80</span>{" "}
-                <span className="text-darkGrayBlueFaded"> / 100</span>
-              </div>
-            </div>
-
-            <div className="resultCard bg-cobaltBlueBcg">
-              <div className="flex gap-3 text-cobaltBlue">
-                <img src={visualImg} alt="visual icon" />
-                <span>Reaction</span>
-              </div>
-              <div>
-                <span className="text-darkGrayBlue">80</span>{" "}
-                <span className="text-darkGrayBlueFaded"> / 100</span>
-              </div>
-            </div>
+          <div>
+            <ul className="flex flex-col gap-3 mb-5">
+              {dataFromServer.map((data) => (
+                <li key={data.category}>
+                  <div
+                    className={cn("resultCard", {
+                      "bg-lightRedBcg": data.category === "Reaction",
+                      "bg-orangeYellowBcg": data.category === "Memory",
+                      "bg-greenTealBcg": data.category === "Verbal",
+                      "bg-cobaltBlueBcg": data.category === "Visual",
+                    })}
+                  >
+                    <div
+                      className={cn("flex gap-3", {
+                        "text-lightRed": data.category === "Reaction",
+                        "text-orangeYellow": data.category === "Memory",
+                        "text-greenTeal": data.category === "Verbal",
+                        "text-cobaltBlue": data.category === "Visual",
+                      })}
+                    >
+                      <img src={data.icon} alt={`${data.category} icon`} />
+                      <span>{data.category}</span>
+                    </div>
+                    <div>
+                      <span className="text-darkGrayBlue font-semibold">
+                        {data.score}
+                      </span>{" "}
+                      <span className="text-darkGrayBlueFaded"> / 100</span>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
           <button className="bg-darkGrayBlue hover:bg-gradient-to-b hover:from-gradientFrom hover:to-gradientTo p-3 text-white font-bold text-lg rounded-3xl cursor-pointer">
             Continue
